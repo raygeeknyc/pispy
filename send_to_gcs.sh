7a887ce1f6ffe534a3faa5cffe6ad71d39bfb52c
Raymond Blum <rsygeeknyc@gmail.com>
2018-03-26 13:03:41 -0400

    Reworked to use ssh and run from motion user

---
+++ send_to_gcs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+GSUTIL="/usr/local/google-cloud-sdk/bin/gsutil"
+m_file=`basename $1`
+m_path=`dirname $1`
+mv "$m_path/$m_file" "$m_path/DONE_$m_file"
+chmod -R 777 $m_path/DONE_*
+logger "sending files for $m_path/DONE_$m_file"
+ssh -o stricthostkeychecking=no -o connecttimeout=2 pi@localhost "${GSUTIL} mv /var/lib/motion/DONE_* gs://laboratory"
+rc=$?
+echo "sent files RC $rc"
+logger "sent files RC $rc"
--- send_videos.sh
+++ send_videos.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
+logger "$0 `whoami`"
 ping -c 1 -W 1 www.google.com
 if [[ $? -ne 0 ]];then
-  logger "No internet, not sending files"
+  logger "no internet, not sending files"
+  echo "no internet, not sending files" >2
   exit -1
 fi
-GSUTIL="/usr/local/google-cloud-sdk/bin/gsutil"
-m_file=`basename $1`
-m_path=`dirname $1`
-mv "$m_path/$m_file" "$m_path/DONE_$m_file"
-chmod -R 777 $m_path/DONE_*
-logger "sending files for $m_path/DONE_$m_file"
-su - pi -c "${GSUTIL} mv /var/lib/motion/DONE_* gs://laboratory/ >/tmp/`basename $0`.out 2>&1"
-logger "sent files RC $?"
+`dirname $0`/send_to_gcs.sh "$1" >/tmp/`basename $0`.out 2>&1
